be532aa8346da6283eaa6b736ef7f0731a278cfc
tak99c023 <tak99c023@gmail.com>
2023-02-21 22:25:54 +0900

    Initial commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export default function Post({ post }) {
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Layout title={post.title}>
+      <p className="m-4">
+        {"ID : "}
+        {post.id}
+      </p>
+      <p className="mb-8 text-xl font-bold">{post.titoe}</p>
+      <p className="px-10">{post.body}</p>
+      <Link href="/blog-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            ariaHidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+            ></path>
+          </svg>
+          <span>Back to blog-page</span>
+        </div>
+      </Link>
+
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { post: post } = await getPostData(params.id);
+  return {
+    props: {
+      post,
+    },
+  };
+}
